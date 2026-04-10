43b05330078aa97eebf428dcef984c379c04924d
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-10 23:29:46 +0700

    Update TaskCard.tsx

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -1,1 +1,12 @@
 
+export default function TaskCard() {
+  return (
+    <div className="bg-white p-4 rounded-2xl shadow space-y-2">
+      <h3 className="font-semibold">Thiết kế logo</h3>
+      <div className="flex justify-between">
+        <span className="text-green-500">50 pts</span>
+        <button className="btn">Apply</button>
+      </div>
+    </div>
+  );
+}
