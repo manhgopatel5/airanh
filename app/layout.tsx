eb887168be60a3af41a3ef7105fd0a41c0a23e00
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-11 02:36:31 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,10 @@ import BottomNav from "@/components/BottomNav";
 export default function RootLayout({ children }: any) {
   return (
     <html lang="vi">
-      <body className="max-w-md mx-auto">
-        {children}
-        <BottomNav />
-      </body>
+      <body className="max-w-md mx-auto pb-24">
+  {children}
+  <BottomNav />
+</body>
     </html>
   );
 }
