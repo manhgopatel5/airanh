850693ffb73019e2a9fb11292d2b96af458ec003
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-10 23:26:49 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,12 @@
 
+import TaskCard from "@/components/TaskCard";
+
+export default function Home() {
+  return (
+    <div className="p-4 space-y-4">
+      <h1 className="text-xl font-bold">🔥 Hot Tasks</h1>
+      <TaskCard />
+      <TaskCard />
+    </div>
+  );
+}
