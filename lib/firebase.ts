c939d55c29f5cf3a884674636a287ed5be3f6426
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-11 00:56:23 +0700

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,12 +1,14 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyB-mUYa7_t4IrePwI5GGCYWGxnKcG0zc_0",
+  authDomain: "airanh-ba64c.firebaseapp.com",
+  projectId: "airanh-ba64c",
+  storageBucket: "airanh-ba64c.firebasestorage.app",
+  messagingSenderId: "236839124077",
+  appId: "1:236839124077:web:bcf03a9721d45386f2d364",
 };
 
 const app = initializeApp(firebaseConfig);
