1e009c4719d45f40a869e40bdc15b6b52e9750e9
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-10 23:30:05 +0700

    Update ChatBubble.tsx

--- components/ChatBubble.tsx
+++ components/ChatBubble.tsx
@@ -1,1 +1,10 @@
 
+export default function ChatBubble({ text, me }: any) {
+  return (
+    <div className={`flex ${me ? "justify-end" : "justify-start"}`}>
+      <div className={`p-2 rounded-xl ${me ? "bg-green-500 text-white" : "bg-gray-200"}`}>
+        {text}
+      </div>
+    </div>
+  );
+}
