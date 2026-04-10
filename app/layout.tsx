e247edd33f6ab8c04d8c515871d7afc01ed1c739
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-10 23:26:24 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,14 @@
 
+import "../styles/globals.css";
+import BottomNav from "@/components/BottomNav";
+
+export default function RootLayout({ children }: any) {
+  return (
+    <html lang="vi">
+      <body className="max-w-md mx-auto">
+        {children}
+        <BottomNav />
+      </body>
+    </html>
+  );
+}
