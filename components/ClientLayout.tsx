fcf5c73395127612a4e2ecf174564be30215af99
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-11 05:18:27 +0700

    Update ClientLayout.tsx

--- components/ClientLayout.tsx
+++ components/ClientLayout.tsx
@@ -1,10 +1,27 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import BottomNav from "@/components/BottomNav";
 
 export default function ClientLayout({ children }: any) {
   const pathname = usePathname();
+  const router = useRouter();
+  const [isLogin, setIsLogin] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+
+    // Nếu chưa login → chặn
+    if (!user && pathname !== "/login" && pathname !== "/register") {
+      router.replace("/login");
+    } else {
+      setIsLogin(true);
+    }
+  }, [pathname]);
+
+  // ⛔ chưa xác định login → không render gì
+  if (isLogin === null) return null;
 
   const hideNav =
     pathname === "/login" ||
