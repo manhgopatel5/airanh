abc35360bc09d27fcb064b5db1f123f552b023b5
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-12 08:25:23 +0700

    Update ClientLayout.tsx

--- components/ClientLayout.tsx
+++ components/ClientLayout.tsx
@@ -1,42 +1,13 @@
 "use client";
 
-import { usePathname, useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 import BottomNav from "@/components/BottomNav";
-import { useAuth } from "@/lib/AuthContext";
 
 export default function ClientLayout({ children }: any) {
   const pathname = usePathname();
-  const router = useRouter();
-  const { user, loading } = useAuth();
 
   const publicRoutes = ["/login", "/register"];
-
-  useEffect(() => {
-    if (loading) return;
-
-    // ❌ chưa login
-    if (!user && !publicRoutes.includes(pathname)) {
-      router.replace("/login");
-      return;
-    }
-
-    // ✅ đã login
-    if (user && publicRoutes.includes(pathname)) {
-      router.replace("/");
-      return;
-    }
-  }, [user, loading, pathname]); // ✅ PHẢI có pathname
-
   const hideNav = publicRoutes.includes(pathname);
-
-  if (loading) {
-    return (
-      <div className="h-screen flex items-center justify-center">
-        Loading...
-      </div>
-    );
-  }
 
   return (
     <div className="pb-20">
