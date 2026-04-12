a3d6db2e8d1521ff6ce55383cec2799be93df74e
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-12 08:52:04 +0700

    Update ClientLayout.tsx

--- components/ClientLayout.tsx
+++ components/ClientLayout.tsx
@@ -2,17 +2,37 @@
 
 import { usePathname } from "next/navigation";
 import BottomNav from "@/components/BottomNav";
+import { useAuth } from "@/lib/AuthContext";
 
 export default function ClientLayout({ children }: any) {
   const pathname = usePathname();
+  const { user, loading } = useAuth();
 
   const publicRoutes = ["/login", "/register"];
-  const hideNav = publicRoutes.includes(pathname);
+  const isPublic = publicRoutes.includes(pathname);
+
+  // ⏳ loading
+  if (loading) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        Loading...
+      </div>
+    );
+  }
+
+  // ❌ chưa login → show login luôn
+  if (!user && !isPublic) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <p>Vui lòng đăng nhập</p>
+      </div>
+    );
+  }
 
   return (
     <div className="pb-20">
       {children}
-      {!hideNav && <BottomNav />}
+      {!isPublic && <BottomNav />}
     </div>
   );
 }
