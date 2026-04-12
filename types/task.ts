78bca5655ea8fb3d88807b2000befac9acf7a8a6
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-13 00:33:29 +0700

    Create task.ts

---
+++ types/task.ts
@@ -0,0 +1,17 @@
+export type Task = {
+  id: string;
+  title?: string;
+  price?: number;
+  likes?: number;
+  joined?: number;
+  totalSlots?: number;
+  createdAt?: {
+    seconds: number;
+  };
+
+  user?: string;
+  userId?: string;
+  avatar?: string;
+  description?: string;
+  deadline?: number;
+};
