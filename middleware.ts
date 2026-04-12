b81f43c1490ff8ddda32ee9d42c4de774d3c2699
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-12 08:29:29 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,24 +2,29 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token"); // 🔥 cookie auth
+  const token = request.cookies.get("token");
+  const { pathname } = request.nextUrl;
 
   const publicRoutes = ["/login", "/register"];
-  const { pathname } = request.nextUrl;
+
+  // ✅ BỎ QUA file hệ thống
+  if (
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/api") ||
+    pathname.includes(".")
+  ) {
+    return NextResponse.next();
+  }
 
   // ❌ chưa login
   if (!token && !publicRoutes.includes(pathname)) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  // ✅ đã login → không cho vào login
+  // ✅ đã login
   if (token && publicRoutes.includes(pathname)) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/((?!_next|favicon.ico).*)"],
-};
