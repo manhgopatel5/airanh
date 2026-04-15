88aa702ed1ac9e0ab51b041de0316c59c5aea9af
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-16 00:23:59 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,34 @@ import "./globals.css";
 import ClientLayout from "@/components/ClientLayout";
 import { AuthProvider } from "@/lib/AuthContext";
 
-export default function RootLayout({ children }: any) {
+export const metadata = {
+  title: "Airanh",
+  description: "Social task app",
+  manifest: "/manifest.json",
+  themeColor: "#0f172a",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="vi">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="apple-mobile-web-app-title" content="Airanh" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+
+        <meta name="theme-color" content="#0f172a" />
+      </head>
+
       <body className="bg-gray-50">
         <AuthProvider>
           <ClientLayout>{children}</ClientLayout>
