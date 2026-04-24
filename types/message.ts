ab42580cd5a05478ed2f24b304e785598276a409
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-24 16:13:06 +0700

    Update message.ts

--- types/message.ts
+++ types/message.ts
@@ -2,14 +2,32 @@ export type Message = {
   id: string;
   chatId: string;
   senderId: string;
+
+  // content
   text?: string;
   image?: string;
   file?: string;
   fileName?: string;
   location?: { lat: number; lng: number };
-  type: "text" | "image" | "file" | "location";
+
+  type: "text" | "image" | "file" | "location" | "video";
+
+  // time
   createdAt: any;
+
+  // read / seen
   seenBy: string[];
-  replyTo?: { id: string; text: string; userName: string };
+
+  // 👇 THÊM (fix lỗi của mày)
+  status?: "sending" | "sent" | "read";
+
+  // reply
+  replyTo?: {
+    id: string;
+    text: string;
+    userName: string;
+  };
+
+  // reactions
   reactions?: Record<string, string>;
 };
