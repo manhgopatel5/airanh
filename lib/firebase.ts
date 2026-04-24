306c64338f7e6a81f7a7381351d82bbfcbbe4802
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-24 19:15:44 +0700

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -56,8 +56,12 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+ ...(process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID && {
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  }),
+ ...(process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL && {
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  }),
 };
 
 /* ================= INIT APP ================= */
@@ -77,7 +81,7 @@ const auth: Auth = getAuth(app);
 
 export const authReady: Promise<void> =
   typeof window!== "undefined"
- ? setPersistence(auth, browserLocalPersistence).catch((e) => {
+? setPersistence(auth, browserLocalPersistence).catch((e) => {
         console.error("Set persistence failed:", e);
       })
     : Promise.resolve();
