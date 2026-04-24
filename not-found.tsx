7255556b253b1653d00905a2db68b98d1f5f74b8
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-24 23:26:17 +0700

    Create not-found.tsx

---
+++ not-found.tsx
@@ -0,0 +1,9 @@
+export const dynamic = 'force-dynamic';
+
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">404 - Không tìm thấy trang</h1>
+    </div>
+  );
+}
