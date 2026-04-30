1efc559feb250e238108cf26023c61fd0d75ff4b
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-30 12:08:27 +0700

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Loading() {
   return (
     <div className="h-screen w-screen bg-white flex items-center justify-center">
-      <Image src="/icon-512.PNG" alt="Loading" width={100} height={100} className="animate-spin" />
+      <Image src="/icon-512.PNG" alt="Loading" width={130} height={130} className="animate-spin" />
     </div>
   )
 }
