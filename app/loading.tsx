53bb615d28cdd821ed2a7fcfd482287739b6269e
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-30 13:39:21 +0700

    Delete loading.tsx

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export default function Loading() {
-  return (
-    <div className="h-screen w-screen bg-white flex items-center justify-center">
-      <Image src="/icon-512.PNG" alt="Loading" width={130} height={130} className="animate-spin" />
-    </div>
-  )
-}
