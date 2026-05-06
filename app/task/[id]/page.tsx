2a7c8d90526dc9f3a8fc14178addce5d6d0fcb63
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-05-06 08:42:21 +0700

    Update page.tsx

--- app/task/[id]/page.tsx
+++ app/task/[id]/page.tsx
@@ -698,6 +698,7 @@ const taskTime = isTask(task) && task.deadline?.seconds
 
 
       <ImageGallery open={showImageGallery!== null} images={task.images || []} initialIndex={showImageGallery || 0} onClose={() => setShowImageGallery(null)} />
+</div>
     </>
   );
 }
