571946305ba2a26a59d9e2d62deaded4e1e3fec8
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-05-07 02:03:13 +0700

    Update page.tsx

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -253,6 +253,7 @@ export default function TasksPage() {
         </div>
       </div>
 
+
       <style jsx global>{`
 .scrollbar-hide::-webkit-scrollbar { display: none; }
 .scrollbar-hide { -ms-overflow-style: none; scrollbar-width: none; }
