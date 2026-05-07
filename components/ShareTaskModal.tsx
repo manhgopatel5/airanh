628f35957fdccf83c3649bfc9e709daa0f4bb8fd
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-05-07 08:17:51 +0700

    Update ShareTaskModal.tsx

--- components/ShareTaskModal.tsx
+++ components/ShareTaskModal.tsx
@@ -145,7 +145,7 @@ await setDoc(
 
   return (
     <AnimatePresence>
-      <div className="fixed inset-0 z-50">
+       <div className="fixed inset-0 z-[999]">
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
