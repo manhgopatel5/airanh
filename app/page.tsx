bf3415ea9b267a24665621d779dc030c8f701050
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-05-09 06:33:13 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = 'force-dynamic';
 import { useEffect, useState, useCallback, useRef, useMemo } from "react";
 import { getFirebaseDB } from "@/lib/firebase";
 import {
