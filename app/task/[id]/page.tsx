6049ff9d5f0e857f056f8bab61a3518252614db3
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-05-10 02:53:17 +0700

    Update page.tsx

--- app/task/[id]/page.tsx
+++ app/task/[id]/page.tsx
@@ -710,6 +710,8 @@ const taskDeadline = isTask(task) && task.deadline?.seconds
   </div>
 </div>
 
+</div>
+
 <div className="px-4 pt-4 pb-2">
               {isOwner? (
                 <div className="rounded-2xl bg-[#F2F2F7] dark:bg-zinc-800 p-4">
