a8f8a9f8692ad066fdf65b69864b049bb631f6d6
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-05-12 02:28:56 +0700

    Update page.tsx

--- app/profile/[uid]/page.tsx
+++ app/profile/[uid]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useParams, useRouter } from "next/navigation";
-import { useEffect, useState, useCallback, useMemo } from "react";
+import { useEffect, useState, useCallback, useMemo, useRef } from "react";
 import {
   doc,
   getDoc,
