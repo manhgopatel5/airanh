c1833d8346638257a9df8d81813523d8a9116580
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-05-16 00:32:50 +0700

    Create index.ts

---
+++ src/assets/lotties/index.ts
@@ -0,0 +1,29 @@
+import celebrate from "./huha-celebrate.json";
+import coinDrop from "./huha-coin-drop.json";
+import empty from "./huha-empty.json";
+import errorShake from "./huha-error-shake.json";
+import idle from "./huha-idle.json";
+import loadingPull from "./huha-loading-pull.json";
+import noWifi from "./huha-no-wifi.json";
+import plan from "./huha-plan.json";
+import searching from "./huha-searching.json";
+import successCheck from "./huha-success-check.json";
+import switchToggle from "./huha-switch.json";
+import task from "./huha-task.json";
+import walletOpen from "./huha-wallet-open.json";
+
+export {
+  celebrate,
+  coinDrop,
+  empty,
+  errorShake,
+  idle,
+  loadingPull,
+  noWifi,
+  plan,
+  searching,
+  successCheck,
+  switchToggle,
+  task,
+  walletOpen,
+};
