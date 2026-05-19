4c9be4fb383821a4a3f50c49da7c50ddc6e003c6
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-05-19 09:16:04 +0700

    Update page.tsx

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -25,7 +25,7 @@ import type { Task } from "@/types/task";
 import TaskCard from "@/components/task/TaskCard";
 import { toast, Toaster } from "sonner";
 import { useAppStore } from "@/store/app";
-import LottiePlayer from "@/components/ui/LottiePlayer";
+import LottiePlayer from "@/components/LottiePlayer";
 import * as L from "@/components/illustrations";
 import { cn } from "@/lib/utils";
 
