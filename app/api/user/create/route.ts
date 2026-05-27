314dba012c57454871eb5dbe55dc3f30ce929d42
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-05-28 06:40:32 +0700

    Update route.ts

--- app/api/user/create/route.ts
+++ app/api/user/create/route.ts
@@ -4,6 +4,7 @@ import { getFirestore, FieldValue } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
 import { nanoid } from 'nanoid';
 
+
 const projectId = process.env.FIREBASE_PROJECT_ID;
 const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
 const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
