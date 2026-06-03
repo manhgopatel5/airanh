6b7f1fbf3500884c0c099f3f2dd02102ed6e44e4
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-06-03 09:26:38 +0700

    Update route.ts

--- app/.well-known/assetlinks.json/route.ts
+++ app/.well-known/assetlinks.json/route.ts
@@ -1,9 +1,19 @@
 export async function GET() {
-  return Response.json([{
-    relation: ["delegate_permission/common.handle_all_urls"],
-    target: {
-      namespace: "web",
-      site: "https://huha.online"
+  return Response.json([
+    {
+      relation: ["delegate_permission/common.handle_all_urls"],
+      target: {
+        namespace: "web",
+        // SỬA: Phải match với domain user truy cập
+        site: "https://huha.online" 
+      }
+    },
+    {
+      relation: ["delegate_permission/common.handle_all_urls"],
+      target: {
+        namespace: "web", 
+        site: "https://www.huha.online" // THÊM DÒNG NÀY
+      }
     }
-  }]);
+  ]);
 }
