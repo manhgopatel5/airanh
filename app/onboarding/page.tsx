541a754b9e5a25fa3a331e943e79fa137913c66b
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-06-03 08:07:05 +0700

    Update page.tsx

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -9,7 +9,7 @@ import { doc, getDoc, serverTimestamp, updateDoc } from "firebase/firestore";
 import { motion } from "framer-motion";
 import { toast } from "sonner";
 import {
-  FiAlertCircle, FiArrowRight, FiUser, FiMail, FiHome,
+  FiAlertCircle, FiArrowRight, FiUser, FiHome,
   FiMapPin, FiCalendar, FiPhone, FiUsers
 } from "react-icons/fi";
 import HuhaLogo from "@/components/brand/HuhaLogo";
