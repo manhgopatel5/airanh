9271b47df3c43c33281550057ea21d9ac1f1038b
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-06-06 22:36:27 +0700

    Update CustomTabBar.tsx

--- components/CustomTabBar.tsx
+++ components/CustomTabBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { useAppStore } from "@/store/app";
 import { useState, useEffect } from "react";
 
