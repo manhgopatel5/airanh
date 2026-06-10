be37b5bf7ade6f579457c4a9fbed19f9e8479c7a
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-06-11 03:33:39 +0700

    Update page.tsx

--- app/admin/events/page.tsx
+++ app/admin/events/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useAuth } from "@/lib/AuthContext";
+
 import { getFirebaseDB } from "@/lib/firebase";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import {
