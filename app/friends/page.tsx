27faa0681f33789152215ffd64a68dc87a3a58f0
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-06-23 01:06:36 +0700

    Update page.tsx

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -5,7 +5,7 @@ import { getFirebaseDB } from "@/lib/firebase";
 import { collection, onSnapshot, doc, getDoc, setDoc, serverTimestamp, query, limit, getDocs } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { FiUsers, FiShield, FiSearch, FiMessageCircle, FiUserX, FiMapPin, FiRefreshCw, FiX } from "react-icons/fi";
-import { RiVipCrownLine, RiUserSearchLine } from "react-icons/ri";
+import { RiVipCrownLine } from "react-icons/ri";
 import { IoStatsChart, IoRibbon } from "react-icons/io5";
 import { ScanLine, SlidersHorizontal } from "lucide-react";
 import { Html5Qrcode } from "html5-qrcode";
