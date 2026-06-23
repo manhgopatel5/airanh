8592abdd02292749676c3dca21ea21e54dd2108c
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-06-24 01:39:53 +0700

    Update page.tsx

--- app/vip/page.tsx
+++ app/vip/page.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/lib/AuthContext";
 import { getFirebaseDB } from "@/lib/firebase";
-import { doc, onSnapshot, updateDoc, serverTimestamp, Timestamp, collection, query, where, orderBy, limit, getDocs } from "firebase/firestore";
+import { doc, onSnapshot, updateDoc, serverTimestamp, Timestamp, collection, where, orderBy, limit, getDocs } from "firebase/firestore";
 import { toast } from "sonner";
 import { Crown } from "lucide-react";
 import {  differenceInDays } from "date-fns";
