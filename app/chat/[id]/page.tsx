df3e3c2e815c6b37e54c0985de88761e84ee19aa
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-06-30 08:58:41 +0700

    Update page.tsx

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1223,9 +1223,10 @@ useEffect(() => {
       <div className="overflow-hidden rounded-2xl shadow-lg">
         <div className="relative h-[150px] w-full bg-zinc-200">
 <img
-  src={`https://image.thum.io/get/width/800/crop/560/auth/xxxxx/https://www.google.com/maps/@${lat},${lng},17z?hl=vi`}
+  src={`https://staticmap.openstreetmap.de/staticmap.php?center=${lat},${lng}&zoom=17&size=800x560&markers=${lat},${lng},red-pushpin`}
   className="w-full h-full object-cover"
   alt=""
+  loading="lazy"
 />
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none">
             <div className="w-9 h-9 bg-white rounded-full shadow-xl flex items-center justify-center">
